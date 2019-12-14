12d295941bc1143a7b9ef580b083ea7b4e65edc8
Cao Vuong Bach <58335662+cvb1994@users.noreply.github.com>
2019-12-14 11:02:37 +0700

    Update LabSe4 BT2.cpp

--- LabSe4 BT2.cpp
+++ LabSe4 BT2.cpp
@@ -1,10 +1,9 @@
 #include <stdio.h>
 int main (){
 	int sl=1,s=0;
-	for (int i=1;i<=4;i++){
+	for (int i=1;i<=100;i++){
 		s=s+sl;
 		sl=sl+2;
-		
 	}
 	printf("Tong 100 so le dau tien lon hon 0: %d",s);
 	return 0;
